27a124e5dca4ec48a50ca00f48e1ff07b13cdbe5
Nayem-khan1 <nayemkhan4496@gmail.com>
2025-07-03 12:09:42 +0600

    add blog images

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -2,7 +2,7 @@ export const blogData = [
   {
     title: "Understanding React Context API",
     slug: "react-context-api",
-    image: "/images/blog/react-context.jpg",
+    image: "/image/blogs/2.png",
     excerpt:
       "React Context API allows you to manage global state easily without prop drilling. This article explains how to use it effectively.",
     date: "July 2, 2025",
@@ -10,7 +10,7 @@ export const blogData = [
   {
     title: "Getting Started with Tailwind CSS",
     slug: "getting-started-tailwind-css",
-    image: "/images/blog/tailwind-css.jpg",
+    image: "/image/blogs/1.png",
     excerpt:
       "Tailwind CSS is a utility-first CSS framework that makes styling easier and faster. Learn the basics and start building modern UI.",
     date: "June 28, 2025",
@@ -18,7 +18,7 @@ export const blogData = [
   {
     title: "Building Responsive Websites",
     slug: "building-responsive-websites",
-    image: "/images/blog/responsive-websites.jpg",
+    image: "/image/blogs/3.png",
     excerpt:
       "Responsive design ensures your website looks great on all devices. Discover the best practices and tools for building responsive sites.",
     date: "June 20, 2025",
